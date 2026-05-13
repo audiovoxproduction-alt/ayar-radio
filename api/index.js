1a5d70a2a3103f575a49434931c0a08ea9797f54
audiovoxproduction-alt <audiovoxproduction@gmail.com>
2026-05-13 16:53:09 +0900

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    version: "1.0",
+    response: {
+      text: "Включаю Айар Радио",
+      end_session: true,
+      directives: {
+        start_stream: {
+          url: "https://c22.radioboss.fm:8213/stream"
+        }
+      }
+    }
+  });
+}
