d2eb257b91f4529898bb2e1646a9c91abd1f32f6
audiovoxproduction-alt <audiovoxproduction@gmail.com>
2026-05-18 02:37:44 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,22 +1,18 @@
 export default async function handler(req, res) {
   return res.status(200).json({
-    version: "1.0",
     response: {
       text: "Запускаю Айар Радио",
-      end_session: true,
       directives: {
         start_audio_player: {
           item: {
             stream: {
               url: "https://ayar-radio.vercel.app/api/stream"
-            },
-            metadata: {
-              title: "Айар Радио",
-              subtitle: "Онлайн радио"
             }
           }
         }
-      }
-    }
+      },
+      end_session: true
+    },
+    version: "1.0"
   });
 }
