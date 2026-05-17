c6d31939555433f7e1fd06a5ccaa4077e50e30f3
audiovoxproduction-alt <audiovoxproduction@gmail.com>
2026-05-18 02:27:29 +0900

    Update stream.js

--- api/stream.js
+++ api/stream.js
@@ -1,10 +1,7 @@
 export default async function handler(req, res) {
-  const streamUrl = "https://c22.radioboss.fm:8213/live";
+  res.writeHead(302, {
+    Location: "https://c22.radioboss.fm:8213/stream"
+  });
 
-  const response = await fetch(streamUrl);
-
-  res.setHeader("Content-Type", "audio/mpeg");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  response.body.pipe(res);
+  res.end();
 }
