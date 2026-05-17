0339c12b838a9c5e84f4d83399861fbe5c12b797
audiovoxproduction-alt <audiovoxproduction@gmail.com>
2026-05-18 01:38:50 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,18 +3,16 @@ export default async function handler(req, res) {
     version: "1.0",
     response: {
       text: "Включаю Айар Радио",
-      directives: [
-        {
-          type: "AudioPlayer.Play",
-          audio_item: {
-            stream: {
-              url: "https://c22.radioboss.fm:8213/stream",
-              offset_ms: 0
-            }
+      end_session: true
+    },
+    directives: {
+      audio_player: {
+        item: {
+          stream: {
+            url: "https://c22.radioboss.fm:8213/stream"
           }
         }
-      ],
-      end_session: true
+      }
     }
   });
 }
