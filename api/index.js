fcb8a539416b85320e16083ffb4a96ee6bf170d6
audiovoxproduction-alt <audiovoxproduction@gmail.com>
2026-05-18 01:40:41 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       audio_player: {
         item: {
           stream: {
-            url: "https://c22.radioboss.fm:8213/stream"
+            url: "https://c22.radioboss.fm:8213/;"
           }
         }
       }
