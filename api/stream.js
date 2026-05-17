b3f27a3f64fd64c83b0db8edebfec3beae13d878
audiovoxproduction-alt <audiovoxproduction@gmail.com>
2026-05-18 02:43:42 +0900

    Update stream.js

--- api/stream.js
+++ api/stream.js
@@ -1,7 +1,13 @@
-export default async function handler(req, res) {
-  res.writeHead(302, {
-    Location: "https://c22.radioboss.fm:8213/stream"
-  });
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "text/html");
 
-  res.end();
+  res.status(200).send(`
+    <html>
+      <body style="margin:0">
+        <audio autoplay controls>
+          <source src="https://c22.radioboss.fm:8213/stream" type="audio/mpeg">
+        </audio>
+      </body>
+    </html>
+  `);
 }
