de6f6a993d646af890a743ff821e71ca7273ab2a
audiovoxproduction-alt <audiovoxproduction@gmail.com>
2026-05-19 02:15:01 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,35 +1,9 @@
 export default async function handler(req, res) {
-  // Разрешаем кросс-доменные запросы от консоли Яндекса
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  // Отвечаем на предварительный OPTIONS-запрос (CORS)
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  // Формируем правильный ответ для Алисы
-  const response = {
+  return res.status(200).json({
+    version: "1.0",
     response: {
-      text: "Запускаю Айар Радио",
-      tts: "Запускаю Айар Радио",
-      directives: [
-        {
-          type: "AudioPlayer.Play",
-          play_direction: "forward",
-          item: {
-            stream: {
-              url: "https://c22.radioboss.fm:8213/radio.mp3"
-            }
-          }
-        }
-      ],
+      text: "Добро пожаловать в Айар Радио.",
       end_session: false
-    },
-    version: "1.0"
-  };
-
-  // Отправляем ответ
-  return res.status(200).json(response);
+    }
+  });
 }
